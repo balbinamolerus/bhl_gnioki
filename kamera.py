2fff12e7f6c307dffcba95bad906aca572d7612f
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-09 01:46:19 +0200

    Pozdrawiamddsh

--- kamera.py
+++ kamera.py
@@ -6,10 +6,10 @@ val = 1
 try:
 	while True:
 		servo.max()
-		time.sleep(0.02)
+		time.sleep(0.1)
 		servo.detach()
 		servo.min()
-		time.sleep(0.02)
+		time.sleep(0.1)
 		servo.detach()
 except KeyboardInterrupt:
 	print("Program stopfgped")
