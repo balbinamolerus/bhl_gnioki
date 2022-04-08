008ce158827b085926ef7acb08d854652aa4352a
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-08 20:58:58 +0200

    Pozdrawiamddsh

--- ldc.py
+++ ldc.py
@@ -3,87 +3,104 @@ import os
 
 picdir = '/home/pi/bhl/bhl_gnioki/pic'
 libdir = '/home/pi/bhl/bhl_gnioki/lib'
-print(libdir)
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-
+import sys
+import os
 import logging
-from waveshare_epd import epd2in13
+from waveshare_epd import epd2in9_V2
 import time
 from PIL import Image, ImageDraw, ImageFont
 import traceback
 
 logging.basicConfig(level=logging.DEBUG)
 
+try:
+    logging.info("epd2in9 V2 Demo")
+    epd = epd2in9_V2.EPD()
 
-logging.info("epd1in02 Demo")
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
 
-epd = epd2in13.EPD()
-logging.info("init and Clear")
-epd.Init()
-epd.Clear()
+    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
 
-# Drawing on the image
-logging.info("1.Drawing on the image...")
-image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    # Drawing on the Horizontal image
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Himage)
+    draw.text((10, 0), 'hello world', font=font24, fill=0)
+    draw.text((10, 20), '2.9inch e-Paper', font=font24, fill=0)
+    draw.text((150, 0), u'微雪电子', font=font24, fill=0)
+    draw.line((20, 50, 70, 100), fill=0)
+    draw.line((70, 50, 20, 100), fill=0)
+    draw.rectangle((20, 50, 70, 100), outline=0)
+    draw.line((165, 50, 165, 100), fill=0)
+    draw.line((140, 75, 190, 75), fill=0)
+    draw.arc((140, 50, 190, 100), 0, 360, fill=0)
+    draw.rectangle((80, 50, 130, 100), fill=0)
+    draw.chord((200, 50, 250, 100), 0, 360, fill=0)
+    # Himage = Himage.transpose(method=Image.ROTATE_180)
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
 
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
+    # Drawing on the Vertical image
+    logging.info("2.Drawing on the Vertical image...")
+    Limage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(Limage)
+    draw.text((2, 0), 'hello world', font=font18, fill=0)
+    draw.text((2, 20), '2.9inch epd', font=font18, fill=0)
+    draw.text((20, 50), u'微雪电子', font=font18, fill=0)
+    draw.line((10, 90, 60, 140), fill=0)
+    draw.line((60, 90, 10, 140), fill=0)
+    draw.rectangle((10, 90, 60, 140), outline=0)
+    draw.line((95, 90, 95, 140), fill=0)
+    draw.line((70, 115, 120, 115), fill=0)
+    draw.arc((70, 90, 120, 140), 0, 360, fill=0)
+    draw.rectangle((10, 150, 60, 200), fill=0)
+    draw.chord((70, 150, 120, 200), 0, 360, fill=0)
+    epd.display(epd.getbuffer(Limage))
+    time.sleep(2)
 
-draw.text((5, 0), 'hello world', font=font, fill=0)
-draw.text((15, 40), u'微雪电子', font=font, fill=0)
-epd.Display(epd.getbuffer(image))
-time.sleep(2)
+    logging.info("3.read bmp file")
+    Himage = Image.open(os.path.join(picdir, '2in9.bmp'))
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
 
-image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(image)
-draw.rectangle((20, 0, 100, 80), fill=0)
-draw.rectangle((22, 2, 98, 78), fill=255)
-draw.chord((22, 2, 98, 78), 0, 360, fill=0)
-draw.chord((24, 4, 96, 76), 0, 360, fill=255)
-draw.line((20, 0, 100, 80), fill=0)
-draw.line((20, 80, 100, 0), fill=0)
-epd.Display(epd.getbuffer(image))
-time.sleep(2)
+    logging.info("4.read bmp file on window")
+    Himage2 = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
+    bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+    Himage2.paste(bmp, (50, 10))
+    epd.display(epd.getbuffer(Himage2))
+    time.sleep(2)
 
-# read bmp file
-logging.info("2.read bmp file...")
-image = Image.open(os.path.join(picdir, '1in02.bmp'))
-epd.Display(epd.getbuffer(image))
-time.sleep(2)
+    # partial update
+    logging.info("5.show time")
+    time_image = Image.new('1', (epd.height, epd.width), 255)
+    time_draw = ImageDraw.Draw(time_image)
+    epd.display_Base(epd.getbuffer(time_image))
+    num = 0
+    while (True):
+        time_draw.rectangle((10, 10, 120, 50), fill=255)
+        time_draw.text((10, 10), time.strftime('%H:%M:%S'), font=font24, fill=0)
+        newimage = time_image.crop([10, 10, 120, 50])
+        time_image.paste(newimage, (10, 10))
+        epd.display_Partial(epd.getbuffer(time_image))
 
-# read bmp file on window
-logging.info("3.read bmp file on window...")
-image1 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
-image1.paste(bmp, (0, 0))
-epd.Display(epd.getbuffer(image1))
-time.sleep(2)
+        num = num + 1
+        if (num == 10):
+            break
 
-# # partial update
-logging.info("4.show time...")
-epd.Clear()
-epd.Partial_Init()
+    logging.info("Clear...")
+    epd.init()
+    epd.Clear(0xFF)
 
-time_image = Image.new('1', (epd.height, epd.width), 255)
-time_draw = ImageDraw.Draw(time_image)
+    logging.info("Goto Sleep...")
+    epd.sleep()
 
-image_old = epd.getbuffer(time_image)
-num = 0
-while (True):
-    time_draw.rectangle((10, 10, 120, 50), fill=255)
-    time_draw.text((10, 10), time.strftime('%H:%M:%S'), font=font, fill=0)
-    newimage = time_image.crop([10, 10, 120, 50])
-    time_image.paste(newimage, (10, 10))
-    epd.DisplayPartial(image_old, epd.getbuffer(time_image))
-    image_old = epd.getbuffer(time_image)
-    num = num + 1
-    if (num == 10):
-        break
+except IOError as e:
+    logging.info(e)
 
-logging.info("Clear...")
-epd.Init()
-epd.Clear()
-
-logging.info("Goto Sleep...")
-epd.Sleep()
+except KeyboardInterrupt:
+    logging.info("ctrl + c:")
+    epd2in9_V2.epdconfig.module_exit()
+    exit()
