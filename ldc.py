14c7e6a010351a6e8e500d77ac52469ebc4947ef
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-08 23:50:56 +0200

    Pozdrawiamddsh

--- ldc.py
+++ ldc.py
@@ -44,7 +44,7 @@ def screen():
     epd.display_Partial(epd.getbuffer(Himage))
     while True:
         if time.strftime('%H:%M')!=lastTime:
-            draw.rectangle((70, 500, 100, 530, fill=255)
+            draw.rectangle((70, 500, 100, 530, fill=255))
             draw.text((70, 500), time.strftime('%H:%M'), font=font24, fill=0)
 
         if alarm:
