4a716bd4acb15430ca2b8af34d5d74ba8f01b1bd
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-09 01:50:40 +0200

    Pozdrawiamddsh

--- kamera.py
+++ kamera.py
@@ -1,15 +1,19 @@
 from gpiozero import Servo
 import time
 
-servo = Servo(14)
-val = 1
-try:
-	while True:
-		servo.max()
-		time.sleep(0.05)
-		servo.detach()
-		servo.min()
-		time.sleep(0.05)
-		servo.detach()
-except KeyboardInterrupt:
-	print("Program stopfgped")
+def powiadomienie():
+	servo = Servo(14)
+	val = 0
+	try:
+		while True:
+			servo.max()
+			time.sleep(0.05)
+			servo.detach()
+			servo.min()
+			time.sleep(0.05)
+			servo.detach()
+			val+=1
+			if val==10:
+				break
+
+powiadomienie()
