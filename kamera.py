3c5600f426d2353d2a12174753c582e7f51d998b
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-09 01:17:52 +0200

    Pozdrawiamddsh

--- kamera.py
+++ kamera.py
@@ -6,10 +6,10 @@ val = -1
 
 try:
 	while True:
-    	servo.value = val
-    	sleep(0.1)
-    	val = val + 0.1
-    	if val > 1:
-        	val = -1
+		servo.value = val
+		sleep(0.1)
+		val = val + 0.1
+		if val > 1:
+			val = -1
 except KeyboardInterrupt:
 	print("Program stopped")
