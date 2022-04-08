e336a8e32b1ede842c87c777a6b27aed79571bde
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-09 01:16:04 +0200

    Pozdrawiamddsh

--- kamera.py
+++ kamera.py
@@ -2,6 +2,14 @@ from gpiozero import Servo
 from time import sleep
 
 servo = Servo(14)
+val = -1
 
-while True:
-    servo.mid()
+try:
+	while True:
+    	servo.value = val
+    	sleep(0.1)
+    	val = val + 0.1
+    	if val > 1:
+        	val = -1
+except KeyboardInterrupt:
+	print("Program stopped")
