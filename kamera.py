a4ce121cf8907d34f60bcc2c7582f267fa7db362
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-09 01:52:16 +0200

    Pozdrawiamddsh

--- kamera.py
+++ kamera.py
@@ -4,16 +4,15 @@ import time
 def powiadomienie():
 	servo = Servo(14)
 	val = 0
-	try:
-		while True:
-			servo.max()
-			time.sleep(0.05)
-			servo.detach()
-			servo.min()
-			time.sleep(0.05)
-			servo.detach()
-			val+=1
-			if val==10:
-				break
+	while True:
+		servo.max()
+		time.sleep(0.05)
+		servo.detach()
+		servo.min()
+		time.sleep(0.05)
+		servo.detach()
+		val+=1
+		if val==10:
+			break
 
 powiadomienie()
