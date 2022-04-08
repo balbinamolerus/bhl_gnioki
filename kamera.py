7e85e809ea32f8e387f67aa008db3304541750b9
balbinamolerus <bmolerus@student.agh.edu.pl>
2022-04-09 01:04:58 +0200

    Pozdrawiamddsh

---
+++ kamera.py
@@ -0,0 +1,20 @@
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(17)
+
+while True:
+    servo.mid()
+    print('mid')
+    sleep(5)
+    x = servo.min()
+    print('min')
+    print(x)
+    sleep(5)
+    servo.mid()
+    print('mid')
+    sleep(5)
+    y = servo.max()
+    print('max')
+    sleep(5)
+    print(y)
