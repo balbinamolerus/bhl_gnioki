07b839042a96742a77be47dd3cc947f7fc83f679
kamil-mackow <kamil.mackow99@gmail.com>
2022-04-09 02:33:27 +0200

    sasaasas

--- SensorsCombined.py
+++ SensorsCombined.py
@@ -126,24 +126,11 @@ while True:
         time.sleep(1)
         p = GPIO.HIGH
     elif gas == GPIO.LOW and last_gas == GPIO.HIGH:
-        GPIO.output(buzzer, GPIO.HIGH)
         client.publish("alarm", "CO2")
-        ctr = 5
-        alert = True
-        while ctr > 0:
-            gas = GPIO.input(switch)
-            p = GPIO.input(play)
-            if gas == GPIO.HIGH or p == GPIO.LOW:
-                alert = False
-                break
-            time.sleep(0.1)
-            ctr -= 0.1
-        if alert:
-            tele.send_message("CO2 detected! contact your daddy")
-        else:
-            GPIO.output(buzzer, GPIO.LOW)
-        time.sleep(1)
-        p = GPIO.HIGH
+        mixer.music.load("/home/pi/Documents/help.mp3")
+        mixer.music.play(loops=2)
+        time.sleep(8)
+        mixer.music.load("/home/pi/Documents/ElevatorMusic.mp3")
     else:
         GPIO.output(buzzer, GPIO.LOW)
 
