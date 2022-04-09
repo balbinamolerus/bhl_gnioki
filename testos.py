159c0a22ede3199c31e23843441cb71dc7541ce9
maciekbaczmanski <cakeisalie16@gmail.com>
2022-04-09 11:54:44 +0200

    1

--- testos.py
+++ testos.py
@@ -1,3 +1,16 @@
-import time
-x = time.strftime('%H:%M')
-print(int(x[-2:]))
+import sqlite3
+# # con = sqlite3.connect('db.sqlite3')
+# # print(con)
+# # print('Connected to database successfully.')
+# #
+# # cur = con.cursor()
+# # cur.execute("SELECT * FROM 'hmi_appointment'")
+# #
+# # rows = cur.fetchall()
+# # for row in rows:
+# #     print(row[2])
+
+con = sqlite3.connect('db.sqlite3')
+cursor = con.cursor()
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
