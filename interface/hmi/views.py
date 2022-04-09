a29fc809fd93b577fb12c3e645ec46b35d011a31
pawel <pawel.machaj13@gmail.com>
2022-04-09 13:16:00 +0200

    alerts.txt

--- interface/hmi/views.py
+++ interface/hmi/views.py
@@ -4,6 +4,13 @@ import time, datetime
 from django.http import HttpResponse
 
 DAYS = ['Poniedzialek', 'Wtorek', 'Sroda', 'Czwartek', 'Piatek', 'Sobota', 'Niedziela']
+POCZATEK = ['Gdyby kózka nie skakała ', 'Gdzie kucharek sześć ', 'Bez pracy ', 'Gdzie kucharek sześć ',
+            'Nie chwal dnia ', 'Dzieci i ryby ', 'Lepszy wróbel w garści ', 'Co ma wisieć ']
+KONIEC = ['to by nóżki nie złamała.', 'tam nie ma co jeść.', 'nie ma kołaczy', 'tam nie ma co jeść.',
+          'przed zachodem słońca.', 'głosu nie mają.', 'niż gołąb na dachu.', 'nie utonie.']
+
+poczatek_dict = {key: value for value, key in enumerate(POCZATEK)}
+koniec_dict = {key: value for value, key in enumerate(POCZATEK)}
 
 def home(request):
     if request.method == 'POST':
@@ -13,12 +20,14 @@ def home(request):
         minuty = request.POST.getlist('minuty')
         app_time = godzina[0] + ':' + minuty[0]
         app = Appointment(name=name, day=day, time=app_time)
+        with open('D:\\bhl_gnioki\\interface\\hmi\\alerts.txt', 'a') as f:
+            f.write(name + ';' + str(godzina[0]) + ':' + str(minuty[0]) + '\n')
         app.save()
+
     format = "%H:%M"
     #Appointment.objects.all().delete()
     appointments = Appointment.objects.all()
     appointments_sorted = sorted(appointments, key=lambda elem: time.strptime(elem.time, format))
-    print(appointments_sorted)
     today = datetime.datetime.today()
     week_day = today.weekday()
     days_sorted = DAYS[week_day:] + DAYS[:week_day]
@@ -27,7 +36,10 @@ def home(request):
         for elem in appointments_sorted:
             if elem.day == day:
                 appointments_sorted_final.append(elem)
-    print(appointments_sorted_final)
 
     return render(request, 'hmi/home.html', {'wydarzenia': appointments_sorted_final[:5]})
-# Create your views here.
+
+
+def gra(request):
+
+    return render(request, 'hmi/gra.html')
