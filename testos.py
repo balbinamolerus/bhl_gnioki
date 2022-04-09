ceea3cab4afcb23d26b4d575947f39c6fc4fd5f7
maciekbaczmanski <cakeisalie16@gmail.com>
2022-04-09 12:42:10 +0200

    1

--- testos.py
+++ testos.py
@@ -1,21 +1,8 @@
-import sqlite3
-# # con = sqlite3.connect('db.sqlite3')
-# # print(con)
-# # print('Connected to database successfully.')
-# #
-# # cur = con.cursor()
-# # cur.execute("SELECT * FROM 'hmi_appointment'")
-# #
-# # rows = cur.fetchall()
-# # for row in rows:
-# #     print(row[2])
-
-con = sqlite3.connect('/home/pi/bhl/bhl_gnioki/interface/interface/db.sqlite3')
-con.row_factory = sqlite3.Row
-cursor = con.cursor()
-con.commit()
-cursor.execute("SELECT * FROM 'hmi_appointment';")
-con.commit()
-print(cursor.fetchall())
-cursor.close()
-con.close()
+path = "testalert.txt"
+with open(path) as file:
+    lines = file.readlines()
+    for line in lines:
+        idx = line.index(';')
+        event = lines[:idx]
+        time = lines[idx+1:]
+        print(event,' - ', time)
