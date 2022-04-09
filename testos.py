06d671ee6ae682b8a3ef652c453695ac3bf5cd0f
maciekbaczmanski <cakeisalie16@gmail.com>
2022-04-09 12:12:12 +0200

    1

--- testos.py
+++ testos.py
@@ -13,7 +13,9 @@ import sqlite3
 con = sqlite3.connect('/home/pi/bhl/bhl_gnioki/interface/interface/db.sqlite3')
 con.row_factory = sqlite3.Row
 cursor = con.cursor()
+con.commit()
 cursor.execute("SELECT name FROM sqlite_master;")
+con.commit()
 print(cursor.fetchall())
 cursor.close()
 con.close()
