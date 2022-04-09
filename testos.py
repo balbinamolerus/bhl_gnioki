15fb74a49cac519943b09fe9ea64757a61872d32
maciekbaczmanski <cakeisalie16@gmail.com>
2022-04-09 12:43:13 +0200

    1

--- testos.py
+++ testos.py
@@ -3,6 +3,6 @@ with open(path) as file:
     lines = file.readlines()
     for line in lines:
         idx = line.index(';')
-        event = lines[:idx]
-        time = lines[idx+1:]
+        event = line[:idx]
+        time = line[idx+1:]
         print(event,' - ', time)
