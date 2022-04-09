2e621ae81a703d68ef007eef8439edfa5461ee06
kamil-mackow <kamil.mackow99@gmail.com>
2022-04-09 02:40:04 +0200

    sasaasas

--- SensorsCombined.py
+++ SensorsCombined.py
@@ -128,7 +128,7 @@ while True:
     elif gas == GPIO.LOW and last_gas == GPIO.HIGH:
         client.publish("alarm", "CO2")
         mixer.music.load("/home/pi/Documents/gas.mp3")
-        time.sleap(0.5)
+        time.sleep(0.5)
         mixer.music.play(loops=2)
         time.sleep(7.5)
         mixer.music.load("/home/pi/Documents/ElevatorMusic.mp3")
